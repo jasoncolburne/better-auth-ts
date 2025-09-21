e76f066236d8a03fa9a8bb2497af1154119f2e7c
Jason Colburne <j.colburne@gmail.com>
2025-09-21 10:27:22 -0300

    format

--- src/tests/api.test.ts
+++ src/tests/api.test.ts
@@ -2,7 +2,7 @@ import { describe, it, expect, vi, beforeEach, afterEach } from 'vitest'
 import { BetterAuthClient, BetterAuthServer } from '../api'
 import { INetwork } from '../interfaces'
 import {
-    ServerAuthenticationKeyStore,
+  ServerAuthenticationKeyStore,
   ServerAuthenticationRegistrationTokenStore,
   ServerPassphraseAuthenticationKeyStore,
   ServerPassphraseRegistrationTokenStore,
