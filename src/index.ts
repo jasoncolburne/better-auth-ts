20972da7795bcba6f4cdb4861eab7a331669f1c2
Jason Colburne <j.colburne@gmail.com>
2025-09-22 04:34:30 -0300

    exports

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 // Export all public APIs
-export * from './api/index';
-export * from './interfaces/index';
-export * from './messages/index';
+export * from './api/index'
+export * from './interfaces/index'
+export * from './messages/index'
 
 // Default export for convenience
-// export { BetterAuthClient as default } from './api/index'
+export { BetterAuthClient as default } from './api/index'
