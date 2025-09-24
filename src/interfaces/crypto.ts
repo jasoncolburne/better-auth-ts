ae5612cfe3f0fb2306bab18052a7b72bcdf70633
Jason Colburne <j.colburne@gmail.com>
2025-09-23 23:28:08 -0300

    refine

--- src/interfaces/crypto.ts
+++ src/interfaces/crypto.ts
@@ -3,19 +3,27 @@ export interface IDigester {
 }
 
 export interface INoncer {
+  // 128 bits of entropy
   generate128(): Promise<string>
 }
 
 export interface IVerifier {
+  // this is typically just a verification algorithm
   verify(message: string, signature: string, publicKey: string): Promise<boolean>
 }
 
 export interface IVerificationKey {
+  // fetches the public key
   public(): Promise<string>
+
+  // returns the algorithm verifier
   verifier(): IVerifier
+
+  // verifies using the verifier and public key, this ia a convenience method
   verify(message: string, signature: string): Promise<boolean>
 }
 
 export interface ISigningKey extends IVerificationKey {
+  // signs with the key it represents (could be backed by an HSM for instance)
   sign(message: string): Promise<string>
 }
