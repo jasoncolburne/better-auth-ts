83266bbf58dba6801af6c6a7d7584656a77afe46
Jason Colburne <j.colburne@gmail.com>
2025-10-08 11:14:43 -0300

    re-encode

--- src/tests/token.test.ts
+++ src/tests/token.test.ts
@@ -1,5 +1,5 @@
 import { describe, it } from 'vitest'
-import { TokenEncoder } from './implementation'
+import { Secp256r1, TokenEncoder } from './implementation'
 import { AccessToken } from '../messages'
 
 interface IMockAccessAttributes {
@@ -11,11 +11,30 @@ class MockAccessAttributes implements IMockAccessAttributes {
 }
 
 describe('tokens', () => {
-  it('can be parsed', async () => {
+  it('can be encoded and decoded', async () => {
     const tokenEncoder = new TokenEncoder()
 
-    const tokenString =
+    const tempTokenString =
       '0IAGTf0y29Ra-8cjCnXS8NlImAi4_KZfaxgr_5iAux1CLoOZ7d5tvFktxb8Xc6pU2pYQkMw0V75fwP537N9dToIyH4sIAAAAAAACA22PXY-iMBSG_wvX203rUBHuOgIDasQ1jC5uNobaKkU-TFtAZ-J_nzoXu8nOnsuT93k_3i3FZc9lzHijhb5ZnoUIiUl_mNkp0isAWHpgCzKMWSaghJvE309VxifT6_no3Nh1G1jfLMZ7ceCGDYJhvIoDqXySVCAcPdfc2VFYlHG-TabDa0leu1NE56Byc8OJv6lB0taqqFx5jGadHfUiTU9OHYrFXp17FmKIdpfMZk80ileGvHS0Eoc5_1P4jVIM1qW92Qb-7keC6-HlxZH-Yjm-Coxilm1Q2-AV3dPO4LLVuRZtE-WqeISHIZDEGWe125Z-BnVHxc9NuQZk3c-XziyS5-2ybt6OpyJ51Faq44xoQ47gCAMEAZykaORh17PR9wnG8PN2RsuvFyFv_yifPGR_UUp-lFwVwRfATSH8n3WutRS001xZ3rt14bI2xcwo9XxbtxV_PHNWi8byfhnznBlkkEJz6_f9fv8A44o2TvkBAAA'
+
+    const tempKey = new Secp256r1()
+    await tempKey.generate()
+
+    const tempToken = await AccessToken.parse<MockAccessAttributes>(tempTokenString, tokenEncoder)
+    const newToken = new AccessToken<MockAccessAttributes>(
+      tempToken.serverIdentity,
+      tempToken.device,
+      tempToken.identity,
+      tempToken.publicKey,
+      tempToken.rotationHash,
+      tempToken.issuedAt,
+      tempToken.expiry,
+      tempToken.refreshExpiry,
+      tempToken.attributes
+    )
+
+    await newToken.sign(tempKey)
+    const tokenString = await newToken.serializeToken(tokenEncoder)
 
     const token = await AccessToken.parse<MockAccessAttributes>(tokenString, tokenEncoder)
 
